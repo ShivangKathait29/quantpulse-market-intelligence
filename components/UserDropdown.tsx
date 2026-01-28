95f9f20c41991a9a1721527d6a8ba8d4a0cb5976
Shivang <shivangkathait29@gmail.com>
2026-01-28 23:18:41 +0530

    Implement Search

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,4 +1,8 @@
-'use client';
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { LogOut } from 'lucide-react'
+
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -6,80 +10,56 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {useRouter} from "next/navigation";
-import { Button } from '@/components/ui/button';
+} from '@/components/ui/dropdown-menu'
+
+import { Button } from '@/components/ui/button'
 import {
     Avatar,
     AvatarFallback,
     AvatarImage,
-} from "@/components/ui/avatar"
-import { LogOut } from 'lucide-react'
-import NavItems from "@/components/NavItems";
-import {signOut} from "better-auth/api";
+} from '@/components/ui/avatar'
 
+import NavItems from '@/components/NavItems'
 
-const UserDropdown = ({user}: {user:User}) => {
-    const router = useRouter();
+type UserDropdownProps = {
+    user: User
+    initialStocks: StockWithWatchlistStatus[]
+}
 
-    const handleSignOut = async () => {
-        await signOut();
+const UserDropdown = ({ user, initialStocks }: UserDropdownProps) => {
+    const router = useRouter()
+
+    const handleSignOut = () => {
         router.push('/sign-in')
     }
-
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="flex items-centregap-3 text-gray-4 hover:text-yellow-500">
+                <Button variant="ghost" className="flex items-center gap-3">
                     <Avatar className="h-8 w-8">
                         <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                            {user.name[0]}
+                        <AvatarFallback>
+                            {user.name?.[0] ?? 'U'}
                         </AvatarFallback>
                     </Avatar>
-                    <div className="hidden md:flex flex-col items-start">
-                        <span className='text-base font-medium text-gray-400'>
-                            {user.name}
-                        </span>
-                    </div>
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="text-gray-400">
-                <DropdownMenuLabel>
 
-                <div className="flex relative items-center gap-3 py-2">
-                    <Avatar className="h-10 w-10">
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                            {user.name[0]}
-                        </AvatarFallback>
-                    </Avatar>
-                    <div className="flex flex-col">
-                        <span className='text-base font-medium text-gray-400'>
-                            {user.name}
-                        </span>
-                        <span className="text-sm text-gray-500">{user.email}</span>
-                    </div>
-                </div>
-                {/*<DropdownMenuLabel>My Account</DropdownMenuLabel>*/}
-                {/*<DropdownMenuSeparator />*/}
-                {/*<DropdownMenuItem>Profile</DropdownMenuItem>*/}
-                {/*<DropdownMenuItem>Billing</DropdownMenuItem>*/}
-                {/*<DropdownMenuItem>Team</DropdownMenuItem>*/}
-                {/*<DropdownMenuItem>Subscription</DropdownMenuItem>*/}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator className="bg-gray-600"/>
-                <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
-                    <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
+            <DropdownMenuContent>
+                <DropdownMenuItem onClick={handleSignOut}>
+                    <LogOut className="mr-2 h-4 w-4" />
                     Logout
                 </DropdownMenuItem>
-                <DropdownMenuSeparator className="hidden sm:block bg-gray-600"/>
+
+                <DropdownMenuSeparator className="sm:hidden" />
+
                 <nav className="sm:hidden">
-                    <NavItems />
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
             </DropdownMenuContent>
         </DropdownMenu>
     )
 }
+
 export default UserDropdown
